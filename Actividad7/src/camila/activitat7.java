0f165a0a807212396cb63ed87437cd679279d481
Camila Addur <camilaaddur@gmail.com>
2023-12-13 19:55:44 +0100

    Agregando inicio y fin

--- Actividad7/src/camila/activitat7.java
+++ Actividad7/src/camila/activitat7.java
@@ -4,11 +4,12 @@ public class activitat7 {
 	
 	    public static void main(String[] args) {
 	        Random rand = new Random();
-	    
+	        System.out.println("vamos a ver los primeros 20 numeros"); 
 	        for (int i = 0; i < 20; i++) {
 	            int numeroAleatorio = rand.nextInt(); 
 	            System.out.println(numeroAleatorio); 
 	        }
+	        System.out.println("FIN"); 
 	    }
 	}
 
