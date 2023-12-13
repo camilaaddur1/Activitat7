a46eaa5c4bc5634669abf7615149e478c6794f5c
Camila Addur <camilaaddur@gmail.com>
2023-12-13 19:51:23 +0100

    Primer commit – nou projecte Java

---
+++ Actividad7/src/camila/activitat7.java
@@ -0,0 +1,14 @@
+package camila;
+import java.util.Random;
+public class activitat7 {
+	
+	    public static void main(String[] args) {
+	        Random rand = new Random();
+	    
+	        for (int i = 0; i < 20; i++) {
+	            int numeroAleatorio = rand.nextInt(); 
+	            System.out.println(numeroAleatorio); 
+	        }
+	    }
+	}
+
